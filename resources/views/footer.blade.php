f1d6e870961891aa1c0a24f2f513fd4706dfedf1
Reynanda MAS <rygamingr@gmail.com>
2023-01-16 01:41:40 +0700

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,123 @@
+<!-- start footer -->
+<footer class="text-center text-lg-start text-muted shadow-nih bg-light">
+    <div class="container">
+        <!-- Section: Social media -->
+        <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+            <!-- Left -->
+            <div class="text-center p-4">
+                <span>Mari Terhubung Dengan Sosial Media Kami</span>
+            </div>
+            <!-- Left -->
+
+            <!-- Right -->
+            <div>
+                <a href="" class="me-4 text-reset">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="fab fa-google"></i>
+                </a>
+                <a href="#" class="me-4 text-reset">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+                <a href="" class="me-4 text-reset">
+                    <i class="fab fa-github"></i>
+                </a>
+            </div>
+            <!-- Right -->
+        </section>
+        <!-- Section: Social media -->
+
+        <!-- Section: Links  -->
+        <section>
+            <div class="container text-center text-md-start mt-5">
+                <!-- Grid row -->
+                <div class="row mt-3">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-10 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <!-- Content -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            <i class="fas fa-gem me-3"></i>Nama Toko
+                        </h6>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam debitis recusandae ea,
+                            dolorum ad tenetur. Ad quos illum dolorem repellendus molestias minima.
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Usefull Link
+                        </h6>
+                        <p>
+                            <a href="#" class="text-reset">Categories</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-reset">Product</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-reset">Coupon</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-reset">Term Of Service</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-10 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Usefull Link
+                        </h6>
+                        <p>
+                            <a href="#" class="text-reset">Categories</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-reset">Product</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-reset">Coupon</a>
+                        </p>
+                        <p>
+                            <a href="#" class="text-reset">Term Of Service</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4">Contact Us</h6>
+                        <p><i class="fas fa-home me-3"></i> KarangKlesem</p>
+                        <p>
+                            <i class="fas fa-envelope me-3"></i>
+                            reyserver28@gmail.com
+                        </p>
+                        <p><i class="fas fa-phone me-3"></i> 0823 2710 1575</p>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!-- Grid row -->
+            </div>
+        </section>
+        <!-- Section: Links  -->
+
+    </div>
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(14, 192, 232, 0.05);">
+        © 2023 Copyright Made With ♥
+        <a class="text-reset" href="/">REYNANDA MAYUDA ATILA SURYA - 20104065 - SE04C</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- end footer -->
